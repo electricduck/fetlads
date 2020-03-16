4121a6bbeed421945aa6a7e8901039d279c186c1
Ducky <ducky@zyr.io>
2020-03-16 00:16:11 +0000

    Fix previous commit

--- src/common/i18n.js
+++ src/common/i18n.js
@@ -33,7 +33,7 @@ const setLanguage = (defaultLanguage, forceLanguage) => {
 }
 
 export const i18n = new VueI18n({
-  locale: setLanguage('en', true),
+  locale: setLanguage('en'),
   fallbackLocale: 'en',
   messages: {
     de, en, es, fr, nl, ru
