e11a658669d31e5ede3d419b0034af68c2b43276
Ducky <ducky@zyr.io>
2020-03-18 19:17:34 +0000

    📝 chore(eslint): ignore errors for 'no-console'

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import { v } from '@/common/variable'
 import { i18n } from '@/common/i18n'
-//import { version } from './common/version'
+import { version } from './common/version'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import {
   faArrowsAltV, faArrowLeft, faAt,
@@ -63,6 +63,6 @@ new Vue({
   render: h => h(App)
 }).$mount('#app')
 
-//console.log('%cFETLADS ', 'font-weight: 900; font-size: 50px;color: #d7ff3f; text-shadow: 3px 3px 0 #101010')
-//console.log('%cVersion ' + version, 'font-weight: bold; color: #8056ff; font-size: 16px;')
-//console.log('%cPowered by Vue and many late nights.', 'color: #1fb281;')
+console.log('%cFETLADS ', 'font-weight: 900; font-size: 50px;color: #d7ff3f; text-shadow: 3px 3px 0 #101010')
+console.log('%cVersion ' + version, 'font-weight: bold; color: #8056ff; font-size: 16px;')
+console.log('%cPowered by Vue and many late nights.', 'color: #1fb281;')
