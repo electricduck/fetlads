441ebe75b32860e07bddd72128ca6d63a123524e
Ducky <ducky@zyr.io>
2020-03-18 03:21:51 +0000

    whoops

--- src/common/i18n.js
+++ src/common/i18n.js
@@ -35,7 +35,7 @@ const setLanguage = (defaultLanguage, forceLanguage) => {
 }
 
 export const i18n = new VueI18n({
-  locale: setLanguage('ru', true),
+  locale: setLanguage('en', false),
   fallbackLocale: 'en',
   messages: {
     de, en, es, fr, ja, nl, ru
