823bd22310ced8884c3a1fe268c2c00026df470f
Ducky <ducky@zyr.io>
2020-03-18 19:24:50 +0000

    🗑 revert: 7959f46

--- src/main.js
+++ src/main.js
@@ -60,10 +60,7 @@ Vue.prototype.$var = v
 new Vue({
   i18n,
   router,
-  render: h => h(App),
-  mounted () {
-    document.dispatchEvent(new Event('render-event'))
-  }
+  render: h => h(App)
 }).$mount('#app')
 
 console.log('%cFETLADS ', 'font-weight: 900; font-size: 50px;color: #d7ff3f; text-shadow: 3px 3px 0 #101010')
